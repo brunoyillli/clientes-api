858332b42d6b4b6ddc024183af512acbce8263ae
Bruno Mendes <wolfteam1910@gmail.com>
2022-08-07 11:29:11 -0300

    busca por nome ou mes de prestador

--- src/main/java/io/github/brunoyillli/clientes/model/repository/ServicoPrestadoRepository.java
+++ src/main/java/io/github/brunoyillli/clientes/model/repository/ServicoPrestadoRepository.java
@@ -1,11 +1,19 @@
 package io.github.brunoyillli.clientes.model.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import io.github.brunoyillli.clientes.model.entity.ServicoPrestado;
 
 @Repository
 public interface ServicoPrestadoRepository extends JpaRepository<ServicoPrestado, Integer>{
+
+	@Query(" select s from ServicoPrestado s join s.cliente c "
+			+ "where upper( c.nome ) like upper( :nome ) or MONTH(s.data) =:mes")
+	List<ServicoPrestado> findByNomeClienteAndMes(@Param("nome") String nome, @Param("mes") Integer mes);
 
 }
--- src/main/java/io/github/brunoyillli/clientes/rest/ServicoPrestadoController.java
+++ src/main/java/io/github/brunoyillli/clientes/rest/ServicoPrestadoController.java
@@ -2,11 +2,14 @@ package io.github.brunoyillli.clientes.rest;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -45,5 +48,13 @@ public class ServicoPrestadoController {
 		servicoPrestado.setValor(bigDecimalConverter.converter(dto.getPreco()));
 		
 		return repository.save(servicoPrestado);
+	}
+	
+	@GetMapping
+	public List<ServicoPrestado> pesquisar(
+			@RequestParam(value = "nome", required = false ) String nome,
+			@RequestParam(value = "mes", required = false  ) Integer mes
+	){
+		return repository.findByNomeClienteAndMes("%" + nome + "%", mes);
 	}
 }
