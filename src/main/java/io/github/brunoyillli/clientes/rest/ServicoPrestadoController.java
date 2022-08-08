6ae8e387f1adc0f7fc9b9a6f586a2d8602eb58e4
Bruno Mendes <wolfteam1910@gmail.com>
2022-08-08 11:36:16 -0300

    ajuste nas mensagens

--- src/main/java/io/github/brunoyillli/clientes/rest/ServicoPrestadoController.java
+++ src/main/java/io/github/brunoyillli/clientes/rest/ServicoPrestadoController.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,7 +35,7 @@ public class ServicoPrestadoController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public ServicoPrestado salvar(@RequestBody ServicoPrestadoDTO dto) {
+	public ServicoPrestado salvar(@RequestBody @Valid ServicoPrestadoDTO dto) {
 		LocalDate data = LocalDate.parse(dto.getData(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 		Integer idCliente = dto.getIdCliente();
 		 
--- src/main/java/io/github/brunoyillli/clientes/rest/dto/ServicoPrestadoDTO.java
+++ src/main/java/io/github/brunoyillli/clientes/rest/dto/ServicoPrestadoDTO.java
@@ -1,13 +1,24 @@
 package io.github.brunoyillli.clientes.rest.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class ServicoPrestadoDTO {
+	
+	@NotEmpty(message = "{campo.descricao.obrigatorio}")
 	private String descricao;
+	
+	@NotEmpty(message = "{campo.preco.obrigatorio}")
 	private String preco;
+	
+	@NotEmpty(message = "{campo.data.obrigatorio}")
 	private String data;
+	
+	@NotNull(message = "{campo.cliente.obrigatorio}")
 	private Integer idCliente;
 }
