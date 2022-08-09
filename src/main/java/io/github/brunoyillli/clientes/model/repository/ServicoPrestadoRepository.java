1ff55a3a6e0dca5d6d4f69b9c2fea681293a6689
Bruno Mendes <wolfteam1910@gmail.com>
2022-08-09 09:41:02 -0300

    ajuste

--- src/main/java/io/github/brunoyillli/clientes/model/repository/ServicoPrestadoRepository.java
+++ src/main/java/io/github/brunoyillli/clientes/model/repository/ServicoPrestadoRepository.java
@@ -13,7 +13,7 @@ import io.github.brunoyillli.clientes.model.entity.ServicoPrestado;
 public interface ServicoPrestadoRepository extends JpaRepository<ServicoPrestado, Integer>{
 
 	@Query(" select s from ServicoPrestado s join s.cliente c "
-			+ "where upper( c.nome ) like upper( :nome ) or MONTH(s.data) =:mes")
+			+ "where upper( c.nome ) like upper( :nome ) and MONTH(s.data) =:mes")
 	List<ServicoPrestado> findByNomeClienteAndMes(@Param("nome") String nome, @Param("mes") Integer mes);
 
 }
