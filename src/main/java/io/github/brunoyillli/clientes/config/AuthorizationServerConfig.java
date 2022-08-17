ef94f7253dad66260ce72aa9981951263a1dfcf2
Bruno Mendes <wolfteam1910@gmail.com>
2022-08-16 22:12:50 -0300

    usando token jwt

--- src/main/java/io/github/brunoyillli/clientes/config/AuthorizationServerConfig.java
+++ src/main/java/io/github/brunoyillli/clientes/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package io.github.brunoyillli.clientes.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,24 +10,35 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
 	
+	@Value("${security.jwt.signing-key}")
+	private String signingKey;
+	
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
 	@Bean
 	public TokenStore tokenStore(){
-		return new InMemoryTokenStore();
+		return new JwtTokenStore(accessTokenConverter());
+	}
+	
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey(signingKey);
+		return tokenConverter;
 	}
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.tokenStore(tokenStore())
+		.accessTokenConverter(accessTokenConverter())
 		.authenticationManager(authenticationManager);
 	}
 	
